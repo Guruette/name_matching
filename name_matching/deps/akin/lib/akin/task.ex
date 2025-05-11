04f0235ea47a98ff479f42b9b51dccac9fec2daf
Artemis Parvizi <artemis@dyad.net>
2025-05-11 19:16:23 +0100

    fix: more clean up

--- name_matching/deps/akin/lib/akin/task.ex
+++
@@ -1,8 +0,0 @@
-defmodule Akin.Task do
-  @moduledoc """
-  API for all string comparison modules.
-  """
-  @callback compare(%Akin.Corpus{}, %Akin.Corpus{}, Keyword.t(any())) :: number() | map()
-  @callback compare(%Akin.Corpus{}, %Akin.Corpus{}) :: number() | map()
-  @optional_callbacks compare: 2
-end
